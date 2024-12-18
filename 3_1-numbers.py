2d5f768af7180adef4ee71f32caf846aa9432e9c
AneliBem <tolya.bem@gmail.com>
2024-12-19 01:49:31 +0200

    first commit

---
+++ 3_1-numbers.py
@@ -0,0 +1,25 @@
+num1 = 2 + 2
+num2 = 50 - 5 * 6
+num3 = (50 - 5 * 6) / 4
+num4 = 8 / 5
+
+print("num1:", num1)
+print("num2:", num2)
+print("num3:", num3)
+print("num4:", num4)
+
+num6 = 17 / 3
+num7 = 17 // 3
+num8 = 17 % 3
+
+print("num6:", num6)
+print("num7:", num7)
+print("num8:", num8)
+
+num9 = 5 ** 6
+print("num9:", num9)
+
+number1 = 20
+number2 = 5 + 9
+num10 = number1 * number2
+print("num10:", num10)
