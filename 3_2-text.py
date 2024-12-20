9b6eda9b221cf0bf80612ef859512d17934efd37
AneliBem <tolya.bem@gmail.com>
2024-12-20 23:46:52 +0200

    first commit

---
+++ 3_2-text.py
@@ -0,0 +1,20 @@
+print("great!")
+print('snake')
+print('doesn\'t')
+print("doesn\'t")
+print("\"Yes,\" they said.")
+print('"Yes," they said.')
+print('"Isn\'t," they said.')
+print(r'C:\some\name') # r щоб вивести рядок без переносу спеціальним символом
+
+print("""\
+Usage: thingy [OPTIONS]
+    -h                         Display this usage message
+    -H hostname                Hostname to connect to
+    
+""")
+print(3 * 'Py' + 'thon')
+print('py' 'thon')
+print("python "
+      "language for programs")
+
