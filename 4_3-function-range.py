77c01cbcdfab7396b2cb438661935211c16f381e
AneliBem <tolya.bem@gmail.com>
2024-12-24 01:53:37 +0200

    first commit

---
+++ 4_3-function-range.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i, end=" ")
+
+print("")
+
+print(list(range(-5, -10, -2)))
+
+a = ["Mary", "had", "a", "little", "lamb"]
+for i in range(len(a)):
+    print(i + 1, a[i])
+
+print(list(enumerate(a)))
+
+print(sum(range(5)))
