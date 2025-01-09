68d5d4ded9a351eedfc262e2e06d6ba6ca4f9599
AneliBem <tolya.bem@gmail.com>
2025-01-09 22:50:39 +0200

    first commit

--- 4_9_2-keyword-arguments.py
+++ 4_9_2-keyword-arguments.py
@@ -18,3 +18,17 @@ parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
 # parrot(110, voltage=220)
 # parrot(actor="John Cleese")
 
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
