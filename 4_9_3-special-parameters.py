633378b92d7d23cb6fadc6d92c02626c572764e5
AneliBem <tolya.bem@gmail.com>
2025-01-12 21:12:45 +0200

    first comit

--- 4_9_3-special-parameters.py
+++ 4_9_3-special-parameters.py
@@ -1,11 +1,31 @@
-def standard_agr(agr): # аргументи і позиційні і ключові
-    print(agr)
+def standard_arg(arg): # аргументи і позиційні і ключові
+    print(arg)
 
-def pos_only_agr(agr, /): # тільки позиційні
-    print(agr)
+def pos_only_arg(arg, /): # тільки позиційні
+    print(arg)
 
-def kwd_only_agr(*, agr): # тільки ключові
-    print(agr)
+def kwd_only_arg(*, arg): # тільки ключові
+    print(arg)
 
 def combined_example(pos_only, /, standard, *, kwd_only): # всі три умови в одній функції
     print(pos_only, standard, kwd_only)
+
+standard_arg(2)
+
+standard_arg(arg=2)
+
+
+pos_only_arg(1)
+
+# pos_only_arg(arg=1) #error
+
+
+# kwd_only_arg(3) #error
+
+kwd_only_arg(arg=3)
+
+
+# combined_example(1, 2, 3) # kwd_only error
+combined_example(1, 2, kwd_only=3)
+combined_example(1, standard=2, kwd_only=3)
+# combined_example(pos_only=1, standard=2, kwd_only=3) # pos_only error
