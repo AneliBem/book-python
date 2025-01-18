4b45aec48bc6b6d3858babf0eebfafee850f8684
AneliBem <tolya.bem@gmail.com>
2025-01-18 21:07:12 +0200

    first commit

--- 4_9_5-unpacking-arg-lists.py
+++ 4_9_5-unpacking-arg-lists.py
@@ -4,3 +4,11 @@ args = [3, 6]
 b = list(range(*args))
 
 print(a, b)
+
+def parrot(voltage, state="a stiff", action="voom"):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
