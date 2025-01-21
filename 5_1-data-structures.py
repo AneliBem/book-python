3f05270e7c00e5f3a65a1d2ad61acefcbc087eff
AneliBem <tolya.bem@gmail.com>
2025-01-21 22:18:33 +0200

    first commit

---
+++ 5_1-data-structures.py
@@ -0,0 +1,12 @@
+fruts = ['banana', 'apple', 'pear', 'banana', 'kiwi', 'orange', 'banana', 'pear']
+print(fruts.count("apple"))
+print(fruts.count("tangerine"))
+print(fruts.index('banana'))
+print(fruts.index('banana', 4))
+fruts.reverse()
+print(fruts)
+fruts.append('grape')
+print(fruts)
+fruts.sort()
+print(fruts)
+print(fruts.pop())
