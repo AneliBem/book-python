56c9f62bbb2b1d7ecdf4c90020170d21e60d126f
AneliBem <tolya.bem@gmail.com>
2025-01-26 22:15:02 +0200

    first commit

---
+++ 6_3-function-dir.py
@@ -0,0 +1,10 @@
+import fibo6, sys
+import builtins
+print(dir(fibo6))
+print(dir(sys))
+
+a = [1, 2, 3, 4, 5]
+fib = fibo6.fib
+print(dir()) # імена визначенні на даний момент
+
+print(dir(builtins)) # список вбудованих функцій та зменних
