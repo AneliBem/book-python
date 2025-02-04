34af66c69c40b0fa3e1b4db641e9b6facf398233
AneliBem <tolya.bem@gmail.com>
2025-02-04 15:57:09 +0200

    first commit

--- 10_1-OS-interface.py
+++ 10_1-OS-interface.py
@@ -1,7 +1,12 @@
 import os
+import shutil
 print(os.getcwd())
 # os.chdir() # змінити каталог
 # os.system('mkdir today') # створення нового каталогу
 
 dir(os) # list available attributes and methods
 help(os) # documentation os
+
+shutil.copyfile("work.txt", "workfile.txt") # копіює вміст файлу work.txt в workfile.txt
+
+# shutil.move('old_folder', 'new_lokation') # переміщає файл або папку
