c2ee60187a14a9e71d664ce36f4518a0c74f4fcc
AneliBem <tolya.bem@gmail.com>
2025-02-04 03:07:21 +0200

    first commit

---
+++ 10_1-OS-interface.py
@@ -0,0 +1,7 @@
+import os
+print(os.getcwd())
+# os.chdir() # змінити каталог
+# os.system('mkdir today') # створення нового каталогу
+
+dir(os) # list available attributes and methods
+help(os) # documentation os
