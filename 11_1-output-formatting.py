1c0594a35b67ed433b5b225dfca9bb033827d950
AneliBem <tolya.bem@gmail.com>
2025-02-06 13:13:25 +0200

    first commit

---
+++ 11_1-output-formatting.py
@@ -0,0 +1,25 @@
+import reprlib
+import pprint
+import textwrap
+import locale
+
+print(reprlib.repr(set('supercalifragilisticexpialidocious')))
+
+
+t = [[[['black', 'cyan'], 'while', ['green', 'red']], [['magenta', 'yellow'], 'blue']]]
+pprint.pprint(t, width=30)
+
+
+doc = """The wrap() method is just like fill() except that it returns 
+a list of strings instead of one big string with newlines to separate
+the wrapped lines."""
+print(textwrap.fill(doc, width=40))
+
+
+print(locale.setlocale(locale.LC_ALL,  'en_US.utf8'))
+conv = locale.localeconv()
+x = 1234567.8
+print(locale.format_string("%d", x, grouping=True))
+print(locale.format_string("%s%.*f", (conv['currency_symbol'],
+                                      conv['frac_digits'], x), grouping=True))
+
